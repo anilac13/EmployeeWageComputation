9232383fdfdcacffc919c6df0c6557d5f5076737
anilac13 <anil4u2496@gmail.com>
2023-04-26 08:34:12 +0530

    [Anil] Add:Check employee is present or not

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,6 +6,7 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+            EmployeeCheck.CheckAttendance();
         }
     }
 }
