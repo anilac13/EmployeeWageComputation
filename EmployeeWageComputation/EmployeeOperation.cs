fecbc1eb0e8dd776de874d1e528cacbd1e45a220
anilac13 <anil4u2496@gmail.com>
2023-04-27 08:37:55 +0530

    [Anil] Add:Calculate employee wage until working days & working hours reaches

--- EmployeeWageComputation/EmployeeOperation.cs
+++ EmployeeWageComputation/EmployeeOperation.cs
@@ -8,13 +8,14 @@ namespace EmployeeWageComputation
 {
     public class EmployeeOperation
     {
-        public const int IS_FULL_TIME = 1, WAGE_PER_HOUR = 20, IS_PART_TIME = 2, WORKING_DAYS = 20;
+        public const int IS_FULL_TIME = 1, WAGE_PER_HOUR = 20, IS_PART_TIME = 2, WORKING_DAYS = 20, TOTAL_WORKING_HRS = 100;
         public static void Operation()
         {
             int empHour = 0;
-            int empWage = 0, totalEmpWage = 0;
-            for (int day=1; day<=WORKING_DAYS; day++)
+            int totalEmpHrs = 0, totalWorkingDays = 0;
+            while(totalEmpHrs < TOTAL_WORKING_HRS && totalWorkingDays < WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(3);
                 switch (empCheck)
@@ -29,11 +30,13 @@ namespace EmployeeWageComputation
                         empHour = 0;
                         break;
                 }
-                empWage = empHour * WAGE_PER_HOUR;
-                Console.WriteLine("Employee Wage is: {0}", empWage);
-                totalEmpWage += empWage;
+                totalEmpHrs += empHour;
+                Console.WriteLine("Day#: "+totalWorkingDays+" EmpHrs: "+empHour);
             }
-            Console.WriteLine("Employee wage for month: {0}", totalEmpWage);
+            Console.WriteLine("Total working days: " + totalEmpHrs);
+            Console.WriteLine("Total working days: " + totalWorkingDays);
+            int totalEmpWage = totalEmpHrs*WORKING_DAYS;
+            Console.WriteLine("Employee wage is: {0}", totalEmpWage);
         }
     }
 }
