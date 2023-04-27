1584c6d20014d06f8fa3ba83bb2a61ed346293e2
anilac13 <anil4u2496@gmail.com>
2023-04-27 08:05:43 +0530

    [Anil] Add:Calculate employee wage for month

--- EmployeeWageComputation/EmployeeOperation.cs
+++ EmployeeWageComputation/EmployeeOperation.cs
@@ -8,27 +8,32 @@ namespace EmployeeWageComputation
 {
     public class EmployeeOperation
     {
-        public const int IS_FULL_TIME = 1, WAGE_PER_HOUR = 20, IS_PART_TIME = 2;
+        public const int IS_FULL_TIME = 1, WAGE_PER_HOUR = 20, IS_PART_TIME = 2, WORKING_DAYS = 20;
         public static void Operation()
         {
             int empHour = 0;
-            int empWage = 0;
+            int empWage = 0, totalEmpWage = 0;
             Random random = new Random();
             int empCheck = random.Next(3);
-            switch (empCheck)
+            for (int day=1; day<=WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    empHour = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHour = 4;
-                    break;
-                default:
-                    empHour = 0;
-                    break;
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHour = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHour = 4;
+                        break;
+                    default:
+                        empHour = 0;
+                        break;
+                }
+                empWage = empHour * WAGE_PER_HOUR;
+                Console.WriteLine("Employee Wage is: {0}", empWage);
+                totalEmpWage += empWage;
             }
-            empWage = empHour * WAGE_PER_HOUR;
-            Console.WriteLine("Employee Wage is: {0}", empWage);
+            Console.WriteLine("Employee wage for month: {0}", totalEmpWage);
         }
     }
 }
