8a85cee2cb20d2aaf349eeab2e79ae8afcbb63bb
anilac13 <anil4u2496@gmail.com>
2023-05-09 19:12:47 +0530

    [Anil] Add:Calculate Emp Wage for multiple companies

---
+++ EmployeeWageComputation/EmpWageBuilderArray.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmpWageBuilderArray
+    {
+        public const int IS_PART_TIME = 2, IS_FULL_TIME = 1;
+        private int numOfCompany = 0;
+        private CompanyEmployeeWage[] companyEmpWageArray;
+        public EmpWageBuilderArray()
+        {
+            this.companyEmpWageArray = new CompanyEmployeeWage[5];
+        }
+        public void AddCompanyEmpWage(string company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth)
+        {
+            companyEmpWageArray[this.numOfCompany] = new CompanyEmployeeWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+            numOfCompany++;
+        }
+        public void ComputeEmpWage()
+        {
+            for (int i = 0; i < numOfCompany; i++)
+            {
+                companyEmpWageArray[i].SetTotalEmpWage(this.ComputeEmpWage(this.companyEmpWageArray[i]));
+                Console.WriteLine(this.companyEmpWageArray[i].ToString());
+            }
+        }
+        public int ComputeEmpWage(CompanyEmployeeWage companyEmpWage)
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs<=companyEmpWage.maxHoursPerMonth && totalWorkingDays<companyEmpWage.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day " + totalWorkingDays + " Emp Hrs: " + empHrs);
+            }
+            return totalEmpHrs * companyEmpWage.empRatePerHour;
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,8 +6,16 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            EmployeeOperation employee = new EmployeeOperation();
-            employee.Operation();
+            //EmployeeOperation employee = new EmployeeOperation();
+            //employee.Operation();
+
+            EmpWageBuilderArray empWage = new EmpWageBuilderArray();
+            empWage.AddCompanyEmpWage("Infosys", 20, 4, 15);
+            empWage.AddCompanyEmpWage("TCS", 30, 2, 10);
+            empWage.AddCompanyEmpWage("CapGemini", 25, 3, 20);
+            empWage.AddCompanyEmpWage("Bosch", 15, 2, 10);
+            empWage.AddCompanyEmpWage("Virtusa", 40, 4, 20);
+            empWage.ComputeEmpWage();
         }
     }
 }
