bf287b8a74dd0c5274151ddaa35b4ce8d1eec8de
anilac13 <anil4u2496@gmail.com>
2023-05-12 13:00:44 +0530

    [Anil] Add:Total wage when queried by company

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,8 +6,6 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            //EmployeeOperation employee = new EmployeeOperation()
-
             EmpWageBuilderArray empWage = new EmpWageBuilderArray();
             empWage.AddCompanyEmpWage("Infosys", 20, 4, 15);
             empWage.AddCompanyEmpWage("TCS", 30, 2, 10);
